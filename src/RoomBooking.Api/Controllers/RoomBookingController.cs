cb77d80d6ddbf71697cb9e1321aabfb558353780
Soe Kyaw Thu <soekyawthu.dev@gmail.com>
2023-10-21 22:34:11 +0630

    Created RoomBooking.Api Project

---
+++ src/RoomBooking.Api/Controllers/RoomBookingController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoomBooking.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomBookingController : ControllerBase
+{
+    
+}
